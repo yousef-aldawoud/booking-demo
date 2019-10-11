54b74764442b5540163473eba572c3435288fe0b
Yousef <yousef.l.t.aldawoud@gmail.com>
2019-10-11 19:45:59 +1300

    Made BookCard

---
+++ src/components/BookCard.vue
@@ -0,0 +1,38 @@
+<template>
+    <b-card>
+        <b-card-text>
+            <h4>Customer name :- {{customerName}}</h4> 
+            <strong>Description</strong>
+            <p>
+                {{description}}
+            </p>
+        </b-card-text>
+
+        <b-card-text>
+            <strong>Booking date:-</strong> {{bookingDate}}
+        </b-card-text>
+        <b-button :variant="colored?'danger':''" class=" button">delete</b-button>
+        <b-button :variant="colored?'primary':''" class=" button">edit</b-button>
+    </b-card>
+</template>
+
+<script>
+export default {
+    props:{
+        colored:{default:true},
+        description:{default:""},
+        bookingDate:{default:""},
+        customerName:{default:""},
+    },
+    data(){
+        return{
+
+        }
+    }
+}
+</script>
+<style>
+.button{
+    margin: 10px;
+}
+</style>
---
+++ src/components/Main.vue
@@ -0,0 +1,35 @@
+<template>
+  <b-container >
+      <b-button class="booking-card" size="lg" :variant="colored?'success':''">Add new booking</b-button>
+      <book-card v-for="(booking,index) in bookings" :key="index" class="booking-card"
+                :description="booking.description" :booking-date="booking.date"
+                :customer-name="booking.customerName"
+      />
+  </b-container>
+</template>
+
+<script>
+import BookCard from './BookCard.vue'
+export default {
+    props:{
+        colored:{default:true}
+    },
+  components: {
+    BookCard
+  },data(){
+      return{
+          bookings:[
+              {date:"2019-11-19",description:"a couple with a one child",customerName:"Zammel"},
+              {date:"2019-11-19",description:"a couple with a one child",customerName:"Zammel"},
+          ]
+      }
+  }
+}
+</script>
+
+<style>
+
+.booking-card{
+    margin-top: 10px;
+}
+</style>
